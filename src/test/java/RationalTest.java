f1cb6fa5fe648ec9d8fbefd25c637409ff2aef38
andrew-kam <andrewkamvik@gmail.com>
2023-04-02 14:57:24 +0300

    add testDefaultConstructor

--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -3,5 +3,17 @@ import static org.junit.Assert.*;
 
 public class RationalTest {
 
+//    Блок проверок конструктора класса
+    @Test
+    public void testDefaultConstructor() {
+    Rational defaultNumber = new Rational();
+    assertEquals("Standard constructor returns wrong numerator", 0, defaultNumber.getNumerator());
+    assertEquals("Standard constructor returns wrong denominator", 1, defaultNumber.getDenominator());
+    }
+
+
+
+
+
 
 }
